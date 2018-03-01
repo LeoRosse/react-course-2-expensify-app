94ec89059a635d46fbc2942b44c0cca2e152e62e
leorosse <leorosse87@gmail.com>
2018-03-01 21:10:41 +0100

    implement remove expense from firebase method

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { removeExpense, editExpense } from '../actions/expenses';
+import { startRemoveExpense, editExpense } from '../actions/expenses';
 
 /* const EditExpensePage = (props) => {
     return (
@@ -40,7 +40,7 @@ export class EditExpensePage extends React.Component {
         this.props.history.push('/')
     };
     onRemove = () => {
-        this.props.removeExpense({ id: this.props.expense.id });
+        this.props.startRemoveExpense({ id: this.props.expense.id });
         this.props.history.push('/')
     };
     render() {
@@ -68,7 +68,7 @@ const mapStateToProps = (state, props) => {
 
 const mapDispatchToProps = (dispatch,props) => ({
     editExpense: (id,expense) => dispatch(editExpense(id, expense)),
-    removeExpense: (data) => dispatch(removeExpense(data))
+    startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
   });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
